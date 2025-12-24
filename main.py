62d3897a92649f6868aaadf10d5218a6640531f2
Weibo0123 <weibolin322@gmail.com>
2025-12-23 20:09:27 -0600

    add the main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import paramiko
-import os
+from ssh_client import ssh_session
+from target_manager import get_target
 
+def main():
+    name, ip, port, user, passwd = get_target()
+    ssh_session(ip, port, user, passwd)
+
+if __name__ == '__main__':
+    main()
